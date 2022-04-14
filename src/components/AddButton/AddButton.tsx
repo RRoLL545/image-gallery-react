2138fafe14b49942e8ae76e4b33c539b5a332e0c
Быков Роман <bykov@softlab.ru>
2022-04-14 16:15:38 +0300

    feat: add and inject AddButton component with styles

---
+++ src/components/AddButton/AddButton.tsx
@@ -0,0 +1,9 @@
+import './AddButton.css'
+
+const AddButton = () => {
+  return (
+    <button className="add-button">+</button>
+  )
+}
+
+export default AddButton;
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,4 @@
+import AddButton from "../AddButton/AddButton"
 import Footer from "../Footer/Footer"
 import Header from "../Header/Header"
 import './App.css'
@@ -9,7 +10,7 @@ const App = () => {
         <main className='main-content'>
           <h2 className='cards-list-heading'>Your images list</h2>
           <div className='image-cards-list'>images list</div>
-          <button>+</button>
+          <AddButton />
         </main>
       <Footer />
     </>
