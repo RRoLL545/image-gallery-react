48797347c2e8c95d38fe4d34992d631cacca3bd5
Быков Роман <bykov@softlab.ru>
2022-04-14 12:21:37 +0300

    init: start application development

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div></div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
